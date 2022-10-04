768340dc547825cdca1915cd69903b7363806304
tcheagow <thiagoapalves2004@gmail.com>
2022-10-03 20:00:09 -0400

    Deletando componente infoPessoais

--- components/cadastro/divInfoPessoais/divInicial.jsx
+++ components/cadastro/divInfoPessoais/divInicial.jsx
@@ -1,12 +1,19 @@
 import DivLogoeNome from "../../ui/divLogo/divLogoeTitulo"
-import DivInfoPessoais from "./divInfoPessoais"
+import Input from "../../ui/divInput/input"
 import styles from "../../../styles/Cadastro.module.css"
 
 export default function DivLogo(){
     return(
         <div className={styles.divInicial}>
             <DivLogoeNome/>
-            <DivInfoPessoais/>
+            <div className={styles.divInfoPessoais}>
+                <p>Informações Pessoais</p>
+                <Input placeholder='Nome' tipo="text"/>
+                <Input placeholder='Email' tipo="email"/>
+                <Input placeholder='Escolaridade' tipo="text"/>
+                <Input placeholder='Senha' tipo="password"/>
+                <Input placeholder='Confirmar Senha' tipo="password"/>
+            </div>
         </div>
     )
 };
