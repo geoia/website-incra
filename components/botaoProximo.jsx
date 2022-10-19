11768dee9bea244bb29f88fc071addccf8cd3373
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-10-19 18:11:06 -0400

    refactor: cria a base do projeto com TS (faltando migrar antigos)

--- components/botaoProximo.jsx
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/botao.module.css';
-import Link from 'next/link';
-export default function Botao(props){
-    return(
-        <button  className={styles.proximo} type='button'><Link href={props.endereco}><a style={{textDecoration:'none', color:'#ffffff'}}>{props.valor}</a></Link></button>
-    )
-}
