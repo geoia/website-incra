e775875cc3392b7ef1b51dec98ed19c371be2dfd
Allan <allan.menchik@hotmail.com>
2022-11-04 10:41:31 -0400

    Primeira implementação api Conexão com banco de dados Output de dados pela api

--- pages/api/queimadas.ts
+++ pages/api/queimadas.ts
@@ -2,13 +2,15 @@ import knex from '../../config/knex.config';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  req.query
   const resultado = await knex
-    // knex.raw permite colocar o mesmo usado no SQL
-    .select(knex.raw(''))
-    // devemos definir qual tabela
-    .from('queimadas_novembro_2022')
-    // podemos colocar a condição usando a construção a sintaxe do 'knex' ou 'raw'
-    .where(knex.raw(''));
+    .raw('SELECT ST_AsGeoJSON(ST_Transform(wkb_geometry, 4326), 6) FROM inferencia_out_2021 WHERE ST_Contains(ST_MakeEnvelope(-58,-22,-57,-21, 4326), ST_Transform(wkb_geometry, 4326))')
+    // // knex.raw permite colocar o mesmo usado no SQL
+    // .select('*')
+    // // devemos definir qual tabela
+    // .from('funciona_pf')
+    // // podemos colocar a condição usando a construção a sintaxe do 'knex' ou 'raw'
+    // //.where(knex.raw(''));
 
-  res.status(200).json({ TODO: true });
+  res.status(200).json({ resultado });
 }
