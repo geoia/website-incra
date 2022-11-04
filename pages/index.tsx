1deb35d7cbc72cf8538cc2ee42d66db31585ae9d
Marcus Gajozo <marcusgajozo@gmail.com>
2022-11-03 20:09:34 -0400

    fix: correção da revisão

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
