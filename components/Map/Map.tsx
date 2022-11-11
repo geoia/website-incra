c9781d6c500ef674923dc12e5b3e5870c27fd8ae
matheus-nantes <matheus.nantes246@gmail.com>
2022-11-11 18:15:59 -0400

    docs(mapa): correção de formatação
    Alterações feitas pelo Prettier ao salvar o arquivo

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  MapContainer,
-  TileLayer,
-  Polygon
-} from 'react-leaflet';
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { statesData } from './teste';
 import { pontos } from './pontos';
@@ -17,55 +13,52 @@ export default function Map() {
       zoom={4}
       zoomControl={false}
       scrollWheelZoom={true}
-      style={{ width: '100vw', height: '100vh',zIndex:'0'}}
+      style={{ width: '100vw', height: '100vh', zIndex: '0' }}
     >
       <TileLayer
         url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWF0aGV1cy1uYW50ZXMiLCJhIjoiY2xhMXpoeTRrMDBvYTNvbWZvZXpua2htOCJ9.PeFH8oujEq1AI6a8-tkk7w"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      {
-        statesData.features.map((state) => {
-          const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
+      {statesData.features.map((state) => {
+        const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
 
-          return (<Polygon
+        return (
+          <Polygon
             pathOptions={{
               fillColor: '#FD8D3C',
               fillOpacity: 0.7,
               weight: 2,
               opacity: 1,
-              color: 'white'
+              color: 'white',
             }}
             positions={coordinates}
             eventHandlers={{
               mouseover: (e) => {
                 const layer = e.target;
                 layer.setStyle({
-                  dashArray: "",
-                  fillColor: "#BD0026",
+                  dashArray: '',
+                  fillColor: '#BD0026',
                   fillOpacity: 0.7,
                   weight: 2,
                   opacity: 1,
-                  color: "white",
-                })
+                  color: 'white',
+                });
               },
               mouseout: (e) => {
                 const layer = e.target;
                 layer.setStyle({
                   fillOpacity: 0.7,
                   weight: 2,
-                  dashArray: "3",
+                  dashArray: '3',
                   color: 'white',
-                  fillColor: '#FD8D3C'
+                  fillColor: '#FD8D3C',
                 });
               },
-              click: (e) => {
-
-              }
+              click: (e) => {},
             }}
-          />)
-        })
-      }
-     
+          />
+        );
+      })}
     </MapContainer>
   );
 }
--- pages/Mapa.tsx
+++ pages/Mapa.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
+import React from 'react';
+import dynamic from 'next/dynamic';
 
 function Mapa() {
-  const Map = React.useMemo(() => dynamic(
-    () => import('../components/Map/Map'), // replace '@components/map' with your component's location
-    { 
-      loading: () => <p>O mapa está carregando</p>,
-      ssr: false // This line is important. It's what prevents server-side render
-    }
-  ), [/* list variables which should trigger a re-render here */])
-  return <Map />
+  const Map = React.useMemo(
+    () =>
+      dynamic(
+        () => import('../components/Map/Map'), // replace '@components/map' with your component's location
+        {
+          loading: () => <p>O mapa está carregando</p>,
+          ssr: false, // This line is important. It's what prevents server-side render
+        }
+      ),
+    [
+      /* list variables which should trigger a re-render here */
+    ]
+  );
+  return <Map />;
 }
 
-export default Mapa
+export default Mapa;
