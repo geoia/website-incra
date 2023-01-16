d071abb5bd000e0a1191da17b7d81a14d52e4519
Marcus Gajozo <marcusgajozo@gmail.com>
2023-01-16 14:58:16 -0400

    feat: remoção do outline dos TextField

--- components/ui/input/TextField.tsx
+++ components/ui/input/TextField.tsx
@@ -3,10 +3,11 @@ import TextField, { TextFieldProps } from '@mui/material/TextField';
 export default function CustomTextField({ sx, ...props }: TextFieldProps) {
   return (
     <TextField
-      variant="outlined"
+      variant="standard"
       size="small"
+      InputProps={{ disableUnderline: true }}
       fullWidth
-      sx={{ background: '#fff', borderRadius: 3, ...sx }}
+      sx={{ padding: '8px 15px 4px 15px', background: '#fff', borderRadius: 3, ...sx }}
       {...props}
     />
   );
