dba499ef0b35ad1725ca58ad593d95977755f060
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2023-02-16 23:56:34 -0400

    feat: conteúdo e imagens dos filtros, apresentação da home, universidade e laboratório

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,6 +9,10 @@ import { Box, Button, Grid, Typography, Avatar, AvatarProps } from '@mui/materia
 import NextLink from 'next/link';
 import logoGeoIA from '../public/logo.ico';
 import sobreAnimais from '../images/sobre-animais.png';
+import infraestrutura from '../images/infraestrutura.png';
+import alagadas from '../images/alagadas.png';
+import queimadas from '../images/queimadas.png';
+import vegetacao from '../images/vegetacao.png';
 
 export default function Home() {
   return (
@@ -58,22 +62,20 @@ export default function Home() {
         >
           <Grid item>
             <Typography variant="h1" sx={{ fontWeight: 600, color: 'white' }}>
-              GeoIA
+              EcoGis
             </Typography>
-            <Typography variant="subtitle1" color={'secondary'} fontWeight={500}>
+            <Typography variant="h6" color={'secondary'} fontWeight={500}>
               Universidade Federal de Mato Grosso do Sul
             </Typography>
           </Grid>
           <Typography variant="h4" color={'white'} fontWeight={500}>
-            <em>Lorem ipsum dolor sit amet consectetur</em>
+            Laboratório Geomática e IA
           </Typography>
           <Typography variant="h3" color={'secondary'} fontWeight={500}>
-            Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor
+            <em>Mapeando a biodiversidade brasileira</em>
           </Typography>
           <Typography variant="body1" color={'secondary'} maxWidth={600}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi aut repellendus expedita
-            cupiditate dolores at nam ipsam minima in? Eveniet dolorum at quidem quaerat suscipit
-            saepe, sed voluptatibus et earum!
+            Projeto de pesquisa e extensão do laboratório Geomática e Inteligência Artificial, da Universidade Federal de Mato Grosso do Sul, para monitoramento e gestão dos biomas do Brasil, por meio da ferramenta WebGis
           </Typography>
         </Grid>
       </Grid>
@@ -104,42 +106,59 @@ export default function Home() {
             }}
           >
             <Grid item sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-              <Imagem src="https://source.unsplash.com/random" />
+              <Image src={queimadas} alt="Queimadas" width={400} height={400} />
               <Grid item>
-                <Typography variant="subtitle1">QUEIMADAS</Typography>
-                <Typography variant="body2">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero fugiat et
-                  temporibus accusamus necessitatibus minima quae sapiente dolor atque nobis sit,
+                <Typography variant="subtitle1" sx={{color:'#FFFFFF'}}>Queimadas</Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  As queimadas são incêndios provocados intencionalmente ou não, que se propagam em áreas florestais, agrícolas ou urbanas, causando danos socioambientais. Elas ocorrem devido à ação humana, seja por desmatamento, limpeza de áreas agrícolas, queima de lixo ou por negligência.
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  Os efeitos das queimadas podem ser devastadores para o meio ambiente, causando a destruição da fauna e flora, a emissão de gases tóxicos na atmosfera e o aumento do aquecimento global. Além disso, as queimadas podem colocar em risco a saúde humana, causando problemas respiratórios e agravando doenças crônicas.
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  As medidas para prevenção e controle das queimadas incluem o desenvolvimento de campanhas de conscientização e educação ambiental da população, a fiscalização das áreas de risco, o monitoramento e o controle de atividades que possam causar incêndios. Além disso, é fundamental a investigação e punição dos responsáveis pelos incêndios, como forma de inibir futuras ocorrências e evitar danos socioambientais.
                 </Typography>
               </Grid>
             </Grid>
             <Grid item sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-              <Avatar sx={{ width: 130, height: 130 }} src="https://source.unsplash.com/random" />
+              <Image src={alagadas} alt="Áreas alagadas" width={400} height={400} />
               <Grid item>
-                <Typography variant="subtitle1">ÁREAS ALAGADAS</Typography>
-                <Typography variant="body2">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero fugiat et
-                  temporibus accusamus necessitatibus minima quae sapiente dolor atque nobis sit,
+                <Typography variant="subtitle1" sx={{color:'#FFFFFF'}}>Áreas Alagadas</Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  As áreas alagadas são ambientes aquáticos que possuem uma grande importância ecológica e econômica, as quais podem ser naturais ou artificiais, e temporárias ou permanentes, como rios, riachos, lagoas, baías e áreas pantanosas. 
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  Elas abrigam uma grande diversidade de espécies animais e vegetais, e fornecem importantes serviços ecossistêmicos, como a regulação do clima, a purificação da água, a manutenção da biodiversidade e a produção de alimentos e recursos naturais. E possuem papel fundamental na proteção contra enchentes, manutenção da qualidade da água, geração de energia, irrigação, pesca, turismo e em atividades importantes econômicas locais e ribeirinhas.
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  No entanto, as áreas alagadas também são vulneráveis a diversos tipos de impactos e as mudanças climáticas têm agravado a situação, com o aumento da frequência e intensidade de eventos extremos, como inundações e secas, que afetam as populações e os ecossistemas locais. Por isso, a gestão adequada das áreas alagadas é fundamental para a conservação da biodiversidade e para a garantia da segurança hídrica e alimentar das populações que dependem desses ecossistemas. Isso inclui a implementação de medidas de conservação, o monitoramento da qualidade da água, o planejamento adequado das atividades econômicas e a adoção de práticas sustentáveis que reduzam os impactos sobre esses ambientes.
                 </Typography>
               </Grid>
             </Grid>
             <Grid item sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-              <Avatar sx={{ width: 130, height: 130 }} src="https://source.unsplash.com/random" />
+              <Image src={infraestrutura} alt="Infraestrutura" width={400} height={400} />
               <Grid item>
-                <Typography variant="subtitle1">INFRAESTRUTURA</Typography>
-                <Typography variant="body2">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero fugiat et
-                  temporibus accusamus necessitatibus minima quae sapiente dolor atque nobis sit,
+                <Typography variant="subtitle1" sx={{color:'#FFFFFF'}}>Infraestrutura</Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  Infraestrutura refere-se a um conjunto de estruturas físicas, sociais e institucionais que formam a base para o desenvolvimento econômico e social de um país, região ou cidade. É composta por diferentes tipos de construções, sistemas e equipamentos que prestam serviços essenciais à sociedade, como transportes, energia, água e saneamento, telecomunicações, edificações e outras instalações.
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  A infraestrutura é fundamental para o funcionamento das cidades e para o desenvolvimento de atividades econômicas, como indústria, comércio, turismo, entre outras. Ela afeta diretamente a qualidade de vida da população e é essencial para garantir a segurança, saúde e bem-estar das pessoas. O investimento em infraestrutura é uma prioridade para muitos países, visando o desenvolvimento econômico e social. A falta de investimento ou a má qualidade da infraestrutura pode levar a problemas como congestionamentos, falta de energia, escassez de água, poluição, acidentes e outros riscos para a população.
                 </Typography>
               </Grid>
             </Grid>
             <Grid item sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-              <Avatar sx={{ width: 130, height: 130 }} src="https://source.unsplash.com/random" />
+             <Image src={vegetacao} alt="Vegetação" width={400} height={400} />
               <Grid item>
-                <Typography variant="subtitle1">VEGETAÇÃO</Typography>
-                <Typography variant="body2">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero fugiat et
-                  temporibus accusamus necessitatibus minima quae sapiente dolor atque nobis sit,
+                <Typography variant="subtitle1" sx={{ marginTop: 1, fontSize: 17, color: '#FFFFFF' }}>Vegetação</Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  A vegetação refere-se ao conjunto de plantas, composta por diversas formas de vida vegetal,  e suas interações em um determinado ambiente, a qual é fundamental para a manutenção do equilíbrio ecológico, fornecendo alimentos, abrigo e proteção para a fauna local, além de ser responsável pela produção de oxigênio e pela absorção de dióxido de carbono da atmosfera, o que ajuda a regular o clima e a qualidade do ar.  
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  No entanto, a vegetação é afetada por atividades humanas, como desmatamento, queimadas, mineração, agricultura intensiva, as quais podem causar impactos negativos no ecossistema, incluindo a perda de biodiversidade, a erosão do solo, a diminuição da qualidade do ar e da água, além de outros problemas ambientais.
+                </Typography>
+                <Typography variant="body2" sx={{color:'#509CBF'}}>
+                  Dessa forma, o monitoramento da vegetação é importante para o planejamento e a implementação de políticas de conservação e preservação ambiental. A tecnologia de sensoriamento remoto, por exemplo, permite a obtenção de informações precisas sobre a cobertura vegetal em uma determinada área, permitindo a identificação de áreas de risco e a elaboração de estratégias para a proteção da vegetação e da fauna associada.
                 </Typography>
               </Grid>
             </Grid>
--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -4,6 +4,9 @@ import { Grid, Typography, Box } from '@mui/material';
 
 import HeaderInstitucional from '../components/ui/header/Header';
 import sobreAnimais from '../images/sobre-animais.png';
+import ufmsClaro from '../images/ufms-escuro.png';
+import laboratorioClaro from '../images/laboratorio-escuro.png';
+
 
 export default function Sobre() {
   return (
@@ -17,7 +20,7 @@ export default function Sobre() {
           <Box>
             <Typography variant="h3" component={'h1'} sx={{color: '#509CBF', fontWeight: 800, marginTop:3}}>Sobre o projeto</Typography>
             <Typography variant="h4" sx={{color: '#A6DFFA', fontWeight: 800, marginTop:3}}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              EcoGis
             </Typography>
             <Typography variant="body2" sx={{color: '#FFF', marginTop:3}}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, unde porro deleniti,
@@ -30,34 +33,42 @@ export default function Sobre() {
       </Grid>
       <Grid container sx={{ height: '100vh' }}>
         <Grid item lg={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Image src={sobreAnimais} alt="Imagens do pantanal" width={400} height={480} />
+          <Grid item>
+            <Image src={ufmsClaro} alt="ufmsEscuro" width={750} height={750} />
+          </Grid>
+          <Grid item>
+            <Image src={laboratorioClaro} alt="LaboratorioClaro" width={750} height={750} />
+          </Grid>
         </Grid>
         <Grid item lg={5} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Box>
-            <Typography variant="h4" component={'h2'} sx={{ fontWeight: 600, color: '#0F1C3C' }}>
-              GeoIA
+            <Typography variant="h5" component={'h2'} sx={{ fontWeight: 600, color: '#0F1C3C' }}>
+              Laboratório Geomática e IA
             </Typography>
-            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 17, color: '#0F1C3C' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, unde porro deleniti,
-              qui dolore officiis omnis rem necessitatibus maiores doloremque non repudiandae,
-              mollitia aperiam quas fugit fuga vel eveniet numquam. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Facere voluptas, tempora nulla quod provident quasi
-
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+            No laboratório de Geomática e Inteligência Artificial, utilizamos técnicas e tecnologias da informação para gestão de dados espaciais, buscando identificar padrões, tendências e relações entre esses dados, com o objetivo de oferecer insights e soluções inovadoras para desafios geográficos complexos.
+            </Typography>
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+              A Geomática engloba diversas áreas do conhecimento, como a cartografia, topografia, sensoriamento remoto e sistemas de informação geográfica (SIG), e tem como objetivo coletar, processar, analisar e gerenciar dados geográficos, por meio de serviços de tecnologia da informação. Essas áreas estão em crescente expansão e desenvolvimento, sendo aplicáveis em uma ampla gama de setores, como planejamento urbano, gestão de recursos naturais, monitoramento ambiental e gerenciamento de desastres, as quais podemos contribuir por meio da identificação de áreas críticas para ações preventivas e da previsão de eventos futuros e apoiar a tomada de decisões informadas e assertivas.
+             </Typography>
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+              Dessa forma, o laboratório de Geomática e Inteligência Artificial propõe projetos de pesquisa e extensão capazes de resolver ou minimizar problemas ambientais e urbanos, como desmatamento, queimadas, falta de planejamento urbano, infraestrutura e saneamento básico do Brasil.
             </Typography>
             <Typography
-              variant="h4"
+              variant="h5"
               component={'h2'}
               sx={{ marginTop: 5, fontWeight: 600, color: '#0F1C3C' }}
             >
               UFMS
             </Typography>
-            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 17, color: '#0F1C3C' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, unde porro deleniti,
-              qui dolore officiis omnis rem necessitatibus maiores doloremque non repudiandae,
-              mollitia aperiam quas fugit fuga vel eveniet numquam. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Facere voluptas, tempora nulla quod provident quasi
-              maiores adipisci nostrum dolorum esse mollitia at, est eveniet ex id eligendi
-
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+              A Universidade Federal de Mato Grosso do Sul (UFMS) é uma instituição de ensino superior pública federal brasileira, que começou sua história em 1962. Contudo, a Universidade ganhou esse nome somente depois da divisão do estado em Mato Grosso e Mato Grosso do Sul. A partir de 1966, novos institutos, faculdades, cursos e departamentos foram criados e, em 1967, a rede de ensino superior do estado teve um crescimento significativo.
+            </Typography>
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+              Hoje, a Universidade está presente direta e indiretamente em um raio de mais de 500 km, que abrange mais de 100 municípios, em que cerca de 15 mil alunos são beneficiados pela instituição e mais de mil professores integram o corpo docente da faculdade. Com campi instalados em diversas cidades do interior, o que contribui para descentralizar o ensino para atender à demanda de várias regiões do estado, por meio de cursos de graduação e pós-graduação, presenciais e a distância, os quais também englobam os cursos de especialização e os programas de mestrado e doutorado. O campus principal e a sede administrativa da UFMS compõem a Cidade Universitária, localizada em Campo Grande, capital do estado. Segundo o Índice Geral de Cursos (IGC), criado pelo Ministério da Educação (MEC), a UFMS é a melhor universidade de Mato Grosso do Sul. Em 2011, ela foi classificada com conceito 4, em uma escala de 1 a 5, e está entre as 100 universidades com maior IGC contínuo. 
+            </Typography>
+            <Typography variant="body2" sx={{ marginTop: 1, fontSize: 14, color: '#0F1C3C' }}>
+             Visando ultrapassar os objetivos essenciais de aprimoramento do ensino e estímulo às atividades de pesquisa e de extensão, a UFMS vem participando do ensino e da preservação dos recursos naturais do meio ambiente, especialmente da fauna e flora do Pantanal, região onde está inserida, e que motiva estudos e pesquisas ecológicas na instituição.
             </Typography>
           </Box>
         </Grid>
