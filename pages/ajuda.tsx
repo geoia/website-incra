818af079705f017367d7788a30a33defb9a12a05
Marcus Gajozo <marcusgajozo@gmail.com>
2023-03-01 21:56:06 -0400

    feat: componentes caixinhas de perguntas e respostas página ajuda

--- pages/ajuda.tsx
+++ pages/ajuda.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Grid, Box, Typography } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Image from 'next/image';
 
 import HeaderInstitucional from '../components/ui/header/Header';
@@ -19,21 +23,52 @@ export default function Ajuda() {
           </Typography>
         </Grid>
         <Grid item lg={10} sx={{ marginTop: 10 }}>
-          <Typography variant="h3" sx={{ fontWeight: 500, marginBottom: 5, color: 'secondary'}}>
+          <Typography variant="h3" sx={{ fontWeight: 500, marginBottom: 5, color: 'secondary', textAlign: 'center' }}>
             Tópicos frequentes
           </Typography>
-          <SelectEscolaridade />
-          <SelectEscolaridade />
-          <SelectEscolaridade />
-          <SelectEscolaridade />
-          <SelectEscolaridade />
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Lorem ipsum dolor sit amet consectetur</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At expedita ex repudiandae eos. <br/>Atque esse vitae beatae dolorum soluta ex fugit impedit aperiam corporis, eveniet quae sit vel, saepe nihil.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion sx={{marginTop: 2}}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Lorem ipsum dolor sit amet consectetur</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus
+                ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
         </Grid>
-        <Grid item lg={8} sx={{marginTop:  5, bgColor: '#000'}}>
+        <Grid item lg={8} sx={{ marginTop: 5, bgColor: '#000' }}>
           {/* <Typography variant='h4' sx={{fontWeight: 500, color: 'secondary'}}>Ainda tem dúvida? <br/> Entre em contato com a gente?</Typography> */}
-          <Grid item sx={{marginTop: 5, display: 'flex', justifyContent: 'space-around'}}>
-            <Grid item sx={{bgcolor: '#FFF', width: 200, height: 200}}>Email</Grid>
-            <Grid item sx={{bgcolor: '#FFF'}}>Telefone</Grid>
-            <Grid item sx={{bgcolor: '#FFF'}}>Formulário</Grid>
+          <Grid item sx={{ marginTop: 5, display: 'flex', justifyContent: 'space-around' }}>
+            <Grid item sx={{ bgcolor: '#FFF', width: 200, height: 200 }}>
+              Email
+            </Grid>
+            <Grid item sx={{ bgcolor: '#FFF' }}>
+              Telefone
+            </Grid>
+            <Grid item sx={{ bgcolor: '#FFF' }}>
+              Formulário
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
