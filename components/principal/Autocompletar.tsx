b4558ef1ca261a13efbac992b1f0f1edbcf7857e
matheus-nantes <matheus.nantes246@gmail.com>
2023-04-03 10:43:23 -0400

    feat(principal): captura do nome do item selecionado no autocomplete
    Quando uma cidade é selecionada, é exibido o código dela no console

--- components/principal/Autocompletar.tsx
+++ components/principal/Autocompletar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAutocomplete } from '@mui/base/AutocompleteUnstyled';
 import { styled } from '@mui/system';
 import Search from '@mui/icons-material/Search';
+import { createFilterOptions } from '@mui/material';
 
 const Input = styled('input')(({ theme }) => ({
   width: '100%',
@@ -44,11 +45,24 @@ const Listbox = styled('ul')(({ theme }) => ({
   },
 }));
 
-export default function Pesquisa() {
+const filtro = createFilterOptions({
+  matchFrom: 'start',
+});
+
+function Enviar(valor: any) {
+  console.log(valor);
+}
+
+export default function Pesquisa(this: any) {
   const { getRootProps, getInputProps, getListboxProps, getOptionProps, groupedOptions } =
     useAutocomplete({
       options: cidades,
       getOptionLabel: (option) => option.nome,
+      filterOptions: filtro,
+      selectOnFocus: true,
+      onInputChange: async (event: object, value: string, reason: string) => {
+        cidades.map((option, index) => (value == option.nome ? Enviar(option.id) : null));
+      },
     });
 
   return (
@@ -88,24 +102,30 @@ export default function Pesquisa() {
 }
 
 const cidades = [
-  { nome: 'Anastácio' },
-  { nome: 'Bodoquena' },
-  { nome: 'Coxim' },
-  { nome: 'Dourados' },
-  { nome: 'Eldorado' },
-  { nome: 'Figueirão' },
-  { nome: 'Naviraí' },
-  { nome: 'São Gabriel do Oeste' },
-  { nome: 'Rio Verde' },
-  { nome: 'Alcinópolis' },
-  { nome: 'Bandeirantes' },
-  { nome: 'Jaraguari' },
-  { nome: 'Sidrolândia' },
-  { nome: 'Maracaju' },
-  { nome: 'Dois Irmãos do Buriti' },
-  { nome: 'Aparecida do Taboado' },
-  { nome: 'Chapadão do Sul' },
-  { nome: 'Bonito' },
-  { nome: 'Camapuã' },
-  { nome: 'Campo Grande' },
+  { nome: 'Anastácio', id: 5000708 },
+  { nome: 'Aquidauana', id: 5001102 },
+  { nome: 'Bodoquena', id: 5002159 },
+  { nome: 'Coxim', id: 5003306 },
+  { nome: 'Dourados', id: 5003702 },
+  { nome: 'Eldorado', id: 5003751 },
+  { nome: 'Figueirão', id: 5003900 },
+  { nome: 'Naviraí', id: 5005707 },
+  { nome: 'São Gabriel do Oeste', id: 5007695 },
+  { nome: 'Rio Verde', id: 5007406 },
+  { nome: 'Alcinópolis', id: 5000252 },
+  { nome: 'Bandeirantes', id: 5001508 },
+  { nome: 'Jaraguari', id: 5004908 },
+  { nome: 'Sidrolândia', id: 5007901 },
+  { nome: 'Maracaju', id: 5005400 },
+  { nome: 'Dois Irmãos do Buriti', id: 5003488 },
+  { nome: 'Aparecida do Taboado', id: 5001003 },
+  { nome: 'Chapadão do Sul', id: 5002951 },
+  { nome: 'Bonito', id: 5002209 },
+  { nome: 'Camapuã', id: 5002605 },
+  { nome: 'Campo Grande', id: 5002704 },
+  { nome: 'Corumbá', id: 5003207 },
 ];
+
+function alterarMunicipio(id: any) {
+  console.log(id);
+}
