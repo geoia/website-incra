76d0fcd6d500cd0cac82cb785518dbfa5e40932a
Marcus Gajozo <marcusgajozo@gmail.com>
2023-05-14 10:55:12 -0400

    style: component paper adicionado na página ajuda para estilização do bloco com informações de email

--- pages/ajuda.tsx
+++ pages/ajuda.tsx
@@ -6,29 +6,19 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import EmailIcon from '@mui/icons-material/Email';
 import Image from 'next/image';
+import Paper from '@mui/material/Paper';
 
 import HeaderInstitucional from '../components/ui/header/Header';
 import plantaDeFundo from '../images/planta-de-fundo.svg';
 
 export default function Ajuda() {
-  const box: SxProps = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    bgcolor: '#FFF',
-    width: 200,
-    height: 200,
-    border: 'none',
-    borderRadius: 10,
-  };
-
   return (
     <>
       <HeaderInstitucional />
       <Box sx={{ position: 'absolute', zIndex: -1, top: '50px', right: 0 }}>
-        <Image src={plantaDeFundo} />
+        <Image src={plantaDeFundo} alt="planta de fundo" />
       </Box>
-      <Container sx={{marginTop: '50px'}}>
+      <Container sx={{ marginTop: '100px' }}>
         <Grid item sx={{ marginTop: 5 }}>
           <Typography variant="h3" sx={{ fontWeight: 500, marginBottom: 5, textAlign: 'center' }}>
             Tópicos frequentes
@@ -112,21 +102,19 @@ export default function Ajuda() {
           </Accordion>
         </Grid>
 
-        <Grid item lg={8} sx={{ marginTop: 5, bgColor: '#000' }}>
+        <Grid lg={8} sx={{ marginTop: 5, bgColor: '#000' }}>
           <Typography variant="h4" sx={{ fontWeight: 500, color: 'secondary' }}>
             Ainda tem dúvida? <br /> Entre em contato com a gente?
           </Typography>
-          <Grid item>
-            <Box sx={box}>
-              <Grid item textAlign={'center'}>
-                <EmailIcon color="secondary" sx={{ fontSize: 60 }} />
-                <Typography variant="body1">Email</Typography>
-                <Typography variant="body2" marginTop={2}>
-                  geoiawebgiz@gmail.com
-                </Typography>
-              </Grid>
-            </Box>
-          </Grid>
+        </Grid>
+        <Grid container justifyContent={'center'} marginTop={5}>
+          <Paper sx={{ maxWidth: '200px', textAlign: 'center', padding: '10px' }}>
+            <EmailIcon color="secondary" sx={{ fontSize: 60 }} />
+            <Typography variant="body1">Email</Typography>
+            <Typography variant="body2" marginTop={2}>
+              geoiawebgiz@gmail.com
+            </Typography>
+          </Paper>
         </Grid>
       </Container>
     </>
