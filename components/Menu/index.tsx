f8d605da12fe0e0190a30a99021ebe952c3884d3
Marcus Gajozo <marcusgajozo@gmail.com>
2023-07-04 16:32:03 -0400

    feat: menu para mobile

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -9,12 +9,14 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
+import CancelIcon from '@mui/icons-material/Cancel';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import logo from '../public/images/logo.png';
 import Logo from '../Logo';
+import { Button } from '@mui/material';
 
 const pages: Array<{ titulo: string; rota: string }> = [
   { titulo: 'Apresentação', rota: '' },
@@ -61,7 +63,7 @@ function GeneralMenu() {
               </Typography>
             </Link>
 
-            <Box sx={{ display: {md: 'flex', xs:'none'}, alignItems: 'center' }}>
+            <Box sx={{ display: { md: 'flex', xs: 'none' }, alignItems: 'center' }}>
               {pages.map((page) => {
                 return (
                   <Link href={`/${page.rota}`} passHref key={page.rota} legacyBehavior>
@@ -88,7 +90,7 @@ function GeneralMenu() {
                 );
               })}
             </Box>
-            <Box sx={{ display: {md: 'none'}}}>
+            <Box sx={{ display: { md: 'none' } }}>
               <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -101,9 +103,71 @@ function GeneralMenu() {
               </IconButton>
             </Box>
             {showMenu && (
-                <Box sx={{position: 'absolute', height: '100vh'}} mt={2} p={2} bgcolor="lightblue">
-                  <p>Div visível!</p>
+              <Box
+                sx={{
+                  position: 'fixed',
+                  width: '100%',
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  left: 0,
+                  top: 0,
+                  zIndex: 2,
+                }}
+                bgcolor="lightblue"
+              >
+                <Box
+                  sx={{
+                    width: '250px',
+                    padding: '20px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: 2,
+                  }}
+                >
+                  {pages.map((page) => {
+                    return (
+                      <Link href={`/${page.rota}`} passHref key={page.rota} legacyBehavior>
+                        <Typography
+                          component="a"
+                          sx={{
+                            textAlign: 'center',
+                            cursor: 'pointer',
+                            fontSize: 18,
+                            width: '100%',
+                            padding: '15px 15px',
+                            textDecoration: 'none',
+                            borderRadius: 5,
+                            color: pathname === `/${page.rota}` ? '#FFF' : '#0F1C3C',
+                            backgroundColor: pathname === `/${page.rota}` ? '#0F1C3C' : '#FFF',
+                            '&:hover': {
+                              borderRadius: pathname === `/${page.rota}` ? 5 : 0,
+                              borderBottom: pathname === `/${page.rota}` ? '' : 4,
+                              borderBottomColor: '#0F1C3C',
+                            },
+                          }}
+                        >
+                          {page.titulo}
+                        </Typography>
+                      </Link>
+                    );
+                  })}
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    color="inherit"
+                    onClick={handleToggleMenu}
+                  >
+                    <CancelIcon />
+                  </IconButton>
                 </Box>
+              </Box>
             )}
           </Box>
         </Toolbar>
