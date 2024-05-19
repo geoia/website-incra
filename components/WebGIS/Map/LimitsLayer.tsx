62347d3dd683c5e9a875b14dcc63bf32b01c14b5
pedro-varela <pedroaugustovarela@outlook.com>
2024-05-18 21:07:34 -0400

    feat(limitslayer): altera o comportamento da visualizacao de limites conforme a camada do mapa
    Foi adicionado uma nova visualizacao dos limites dos municipios de acordo com a camada
    escolhida(mais escura quando na camada clara, mais clara na camada escura) Tornando a visualizacao
    possivel quando na camada escura
    
    #52

--- components/WebGIS/Map/LimitsLayer.tsx
+++ components/WebGIS/Map/LimitsLayer.tsx
@@ -3,7 +3,11 @@ import { useEffect } from 'react';
 import useLimitesMunicipios from '../../../hooks/useLimitesMunicipios';
 import L from 'leaflet';
 
-export function LimitsLayer(props: { municipio: number; showLimitVisibility: boolean }) {
+export function LimitsLayer(props: {
+  municipio: number;
+  showLimitVisibility: boolean;
+  showSatellite: boolean;
+}) {
   const events = useMapEvents({});
   const { data: limitesMunicipais } = useLimitesMunicipios(props.municipio);
 
@@ -18,11 +22,11 @@ export function LimitsLayer(props: { municipio: number; showLimitVisibility: boo
       data={limitesMunicipais}
       pathOptions={{
         dashArray: '0',
-        fillColor: '#000000',
-        fillOpacity: props.showLimitVisibility ? 0 : 0.1,
+        fillColor: props.showSatellite ? 'white' : '#000000',
+        fillOpacity: props.showLimitVisibility ? 0 : 0.2,
         weight: props.showLimitVisibility ? 0 : 2,
         opacity: 1,
-        color: '#4f4f4f',
+        color: props.showSatellite ? '#CCCCCC' : '#4f4f4f',
       }}
       eventHandlers={{
         mouseover: (e) => {
