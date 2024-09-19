f5ba8490c810e72faf04e2fc24884ead3703641c
Pedro Nicoletti <pedrosotoma@gmail.com>
2024-09-19 16:38:25 -0400

    feat: adicionando valor default na página de estatísticas

--- pages/estatisticas.tsx
+++ pages/estatisticas.tsx
@@ -18,7 +18,7 @@ function isValidParam(param: any): boolean {
 }
 
 export const getServerSideProps = (async ({ query }) => {
-  const props: { estadoId?: string; municipioId?: string; biomaId?: string } = {};
+  const props: { estadoId?: string; municipioId?: string; biomaId?: string } = {municipioId: "5003207"};
 
   if (isValidParam(query.estadoId)) props.estadoId = query.estadoId as string;
   if (isValidParam(query.municipioId)) props.municipioId = query.municipioId as string;
